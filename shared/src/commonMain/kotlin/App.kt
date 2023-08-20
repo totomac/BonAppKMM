523859b00aeb9ea97e64ec83f6e9db2dbaee785b
Thomas <thomas.macquart@ridedott.com>
2023-08-20 21:11:00 +0200

    feat: improve UI

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,10 +1,17 @@
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
 import org.jetbrains.compose.resources.ExperimentalResourceApi
@@ -25,8 +32,15 @@ fun RecipesPage(viewModel: ListViewModel) {
     val uiModel by viewModel.uiModel.collectAsState(initial = UiModel(emptyList()))
     LazyColumn {
         items(uiModel.recipes) {
-            Text(it.title)
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = it.title,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+            )
             Text(it.description)
+            Divider()
         }
     }
 
